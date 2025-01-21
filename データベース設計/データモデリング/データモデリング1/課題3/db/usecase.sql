aff621a16669b39b5b37c505fdc74cc77b379376
ISAWASHUN <syun20000616@icloud.com>
2025-01-21 22:01:45 +0900

    ✨Database Modeling1 1

--- データベース設計/データモデリング/データモデリング1/課題3/db/usecase.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  parent.item_name AS set_menu_name,
-  child.item_name  AS included_item,
-  mic.child_quantity
-FROM menu_item_components mic
-JOIN menu_items parent ON mic.parent_item_id = parent.menu_item_id
-JOIN menu_items child  ON mic.child_item_id = child.menu_item_id
-WHERE parent.menu_item_id = 5;
