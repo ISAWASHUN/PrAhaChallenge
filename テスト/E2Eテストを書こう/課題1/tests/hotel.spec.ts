58a4baabf8576e3d6f237633964abbc1f45b6831
ISAWASHUN <syun20000616@icloud.com>
2025-05-19 21:47:00 +0900

    ✨feat: 課題1-3の完了

--- テスト/E2Eテストを書こう/課題1/tests/hotel.spec.ts
+++ テスト/E2Eテストを書こう/課題1/tests/hotel.spec.ts
@@ -1,8 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-// 課題1-2
-test('Hotel site トップページが読み込める', async ({ page }) => {
-  await page.goto('https://hotel.testplanisphere.dev/ja/');
+test.describe('Hotel Test Planisphere サイト', () => {
+  // 課題1-2
+  test('トップページにアクセスできる', async ({ page }) => {
+    await page.goto('https://hotel.testplanisphere.dev/ja/');
+    await expect(page).toHaveTitle(/HOTEL PLANISPHERE - テスト自動化練習サイト/i);
+  });
 
-  await expect(page).toHaveTitle(/HOTEL PLANISPHERE - テスト自動化練習サイト/i);
+  // 課題1-3
+  test('会員登録ページに移動できる', async ({ page }) => {
+    await page.goto('https://hotel-example-site.takeyaqa.dev/ja/signup.html');
+
+    await expect(page).toHaveURL(/\/ja\/signup\.html$/);
+    const heading = page.locator('h1');
+    await expect(heading).toHaveText(/Hotel Planisphere/);
+  });
 });
